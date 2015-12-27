563123cb307aad3efe54c6b4da5b77b777627131
Fernando J Pando <fernando.pando@stelligent.com>
2015-12-27 17:29:48 -0500

    docroot recipe

--- cookbook_www/recipes/default.rb
+++ cookbook_www/recipes/default.rb
@@ -4,5 +4,15 @@ package 'php-fpm'
 package 'jwhois'
 
 template '/etc/php-fpm.d/www.conf' do
+  owner 'root'
+  group 'root'
+  mode '0644'
   source 'www.conf.erb'
+end
+
+directory '/var/www' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
 end
